30d5109edf2bdb8f66ac614f04578c4b93697449
CainChu <cain@cainplay.com>
2017-04-24 11:22:06 +0800

    adm-zip

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const unzip = require('unzip')
-const fs = require('fs')
-// fs.createReadStream('a.zip').pipe(unzip.Extract({ path: 'a' }))
-fs.createReadStream('R360.zip').pipe(unzip.Extract({ path: 'b' }))
-    .on('close', (e) => {
-        console.log('end')
-    })
-    .on('error', console.error)
+const AdmZip = require('adm-zip')
+// reading archives 
+var zip = new AdmZip('./R360.zip')
+var zipEntries = zip.getEntries(); // an array of ZipEntry records 
+zipEntries.forEach(function (zipEntry) {
+    console.log(zipEntry.toString()); // outputs zip entries information 
+});
+zip.extractAllTo(/*target path*/'b', /*overwrite*/true);
